7b036efba306fd0027e9222232bc89fc716a049e
Amrit Bhogal <ambhogal01@gmail.com>
2025-02-03 22:20:53 +0000

    make password nullable

--- database/migrations/2025_02_03_205212_create_users_table.php
+++ database/migrations/2025_02_03_205212_create_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_admin')->default(false);
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('ip')->nullable();
             $table->boolean('is_banned')->default(false);
             $table->rememberToken();
